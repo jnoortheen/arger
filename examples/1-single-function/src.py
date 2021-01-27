55808af18803b97ae2f5d7d56faaf8d060700fb5
jnoortheen <jnoortheen@users.noreply.github.com>
2021-01-27 08:46:05 +0000

    Deployed d7648df with MkDocs version: 1.1.2

---
+++ examples/1-single-function/src.py
@@ -0,0 +1,23 @@
+from arger import Arger
+from argparse import Namespace
+from tests.utils import _reprint
+
+
+def main(param1: int, param2: str, _namespace_: Namespace, kw1=None, kw2=False):
+    """Example function with types documented in the docstring.
+
+    :param param1: The first parameter.
+    :param param2: The second parameter.
+    :param kw1: this is optional parameter.
+    :param kw2: this is boolean. setting flag sets True.
+    """
+    _reprint(**locals())
+
+
+arger = Arger(
+    main,
+    prog='pytest',  # for testing purpose. otherwise not required
+)
+
+if __name__ == '__main__':
+    arger.run()
