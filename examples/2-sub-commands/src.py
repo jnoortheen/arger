afbf3f4c19b03ac8b569d9004618537a7df73e7f
jnoortheen <jnoortheen@users.noreply.github.com>
2021-10-16 10:16:07 +0000

    Deployed 8353d5a with MkDocs version: 1.2.3

---
+++ examples/2-sub-commands/src.py
@@ -0,0 +1,38 @@
+from arger import Arger
+from tests.utils import _reprint
+
+
+arger = Arger(prog='pytest', description="App Description goes here")
+
+container = []
+
+
+@arger.add_cmd
+def create(name: str):
+    """Create new test.
+
+    :param name: Name of the test
+    """
+    container.append(container)
+    _reprint(**locals())
+
+
+@arger.add_cmd
+def remove(*name: str):
+    """Remove a test with variadic argument.
+
+    :param name: tests to remove
+    """
+    if name in container:
+        container.remove(remove)
+    _reprint(**locals())
+
+
+@arger.add_cmd
+def list():
+    """List all tests."""
+    _reprint(**locals(), container=container)
+
+
+if __name__ == '__main__':
+    arger.run()
---
+++ examples/4-supported-types/src.py
@@ -0,0 +1,54 @@
+from enum import Enum
+from typing import List, Tuple
+
+from arger import Arger
+from tests.utils import _reprint
+
+
+class Choice(Enum):
+    one = '1. One'
+    two = '2. Two'
+
+    def __str__(self):
+        """by implementing this function one can control the enum-display in help string"""
+        return self.name
+
+arger = Arger(prog='pytest', description="App Description goes here")
+
+container = []
+
+
+@arger.add_cmd
+def cmd1(
+    an_int: int,
+    an_str: str,
+    a_tuple: Tuple[str, str, str],  # nargs: 3 -> consume 3
+    a_var_tuple: Tuple[str, ...],  # nargs: +  -> consume one or more
+    an_enum=Choice.one,
+    optional_str='',
+    optional_int=0,
+    optional_tpl=(),
+):
+    """Example function with types documented in the docstring."""
+    _reprint(**locals())
+
+
+@arger.add_cmd
+def cmd2(
+    a_list: List[int],  # nargs='*': -> capture many args
+    m_opt=False,
+    y_opt=False,
+    my=False,
+):
+    """A script with three optional values.
+
+    :param a_list: catch all positional arguments
+    :param m_opt: the m_opt helptext
+    :param y_opt: the y_opt helptext
+    :param my: the my helptext
+    """
+    _reprint(**locals())
+
+
+if __name__ == '__main__':
+    arger.run()
